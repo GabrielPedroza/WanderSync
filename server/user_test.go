bb6c2ecf0ebf1bbe49f2d64e8918db595e1488dd
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2025-03-08 17:16:49 -0500

    init without graphql template
    there were some issues with trying to generate new schemas so i decided
    to just use the starter code from https://entgo.io/docs/getting-started
    so it can just work.
    
    i will start building on top of it and hopefully later on, i try to get
    the graphql with relay working ideally with graphiql so i can test
    queries

---
+++ server/user_test.go
@@ -0,0 +1,34 @@
+package todo
+
+import (
+	"GabrielPedroza/WanderSync/ent"
+	"context"
+	"fmt"
+	"log"
+
+	"entgo.io/ent/dialect"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func User_Test() {
+	// Create an ent.Client with in-memory SQLite database.
+	client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	ctx := context.Background()
+	// Run the automatic migration tool to create all schema resources.
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	task1, err := client.User.Create().SetName("Gabriel").SetAge(23).Save(ctx)
+	if err != nil {
+		log.Fatalf("failed creating a todo: %v", err)
+	}
+
+	fmt.Println(task1)
+	// Output:
+	// User(id=1, name=Gabriel, age=23)
+}
