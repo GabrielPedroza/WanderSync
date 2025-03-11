422e26daceffe03c36309252ea68b39dc8e7f975
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2025-03-10 20:26:07 -0400

    project is now running smoothly (we can now see graphiql)
    next steps is to add some data to the database and actually try to get
    some back returned to us in graphiql

--- server/resolver.go
+++ server/resolver.go
@@ -4,4 +4,18 @@ package user
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+import (
+	"GabrielPedroza/WanderSync/ent"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
+// Resolver is the resolver root.
+type Resolver struct{ client *ent.Client }
+
+// NewSchema creates a graphql executable schema.
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: &Resolver{client},
+	})
+}
